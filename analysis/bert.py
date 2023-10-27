808f622e85a89f4a8201b376e2bf4a6d46a2d737
sdoosti <shahryar65@gmail.com>
2023-10-27 11:06:09 -0700

    comment text analysis

---
+++ analysis/bert.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 10/26/23
+
+@author: Shahryar Doosti
+
+BERT Analysis on Youtube Comments
+"""
+
+import pandas as pd
+import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+from datetime import date
+import re
+
+today = date.today()
+today_str = today.strftime("%m%d%y")
+
+PATH = os.path.abspath(os.getcwd())
+
+processed_path = "processed_comments_102423.txt"
+comments_path = "merged_comments.csv"
+
+threshold = 10 # minimum number of terms for each comments (after preprocessing)
+
+def data_file_path(file_path):
+    return os.path.join(PATH.replace("API","Data"),file_path)
+
+with open(data_file_path(processed_path),"r") as f:
+    processed_docs = f.readlines()
+comments = pd.read_csv(data_file_path(comments_path))
+comments = comments[comments.comment_text.notnull()].copy()
+comments['processed_text'] = [re.sub("\d+", "", x.strip())for x in processed_docs]
+comments['length'] = comments.processed_text.apply(lambda x: len(x.split(',')))
+comments['include'] = comments.length > threshold
+
+# TF-IDF
+docs = comments[comments.include].processed_text.to_list()
+# create object
+tfidf = TfidfVectorizer(max_features=20000, min_df = 0.001, max_df=0.1)
+ 
+# get tf-df values
+result = tfidf.fit_transform([' '.join(sent.split(',')) for sent in docs])
+
+# get indexing
+print('\nWord indexes:')
+print(tfidf.vocabulary_)
+
+# display tf-idf values
+print('\ntf-idf value:')
+print(result)
+
+# in matrix form
+print('\ntf-idf values in matrix form:')
+print(result.toarray())
+
+tfidf.get_feature_names()
+
+cleaned_docs = []
+for doc in [sent.split(',') for sent in docs]:
+    cleaned_words = [word for word in doc if word in tfidf.vocabulary_]
+    cleaned_docs.append(cleaned_words)
+
+# topic model
+from sklearn.preprocessing import Normalizer
+normalizer = Normalizer(norm='l2')
+tfidf_norm = normalizer.transform(result)
+
+from sklearn.decomposition import LatentDirichletAllocation
+
+lda = LatentDirichletAllocation(n_components=10) 
+lda.fit(tfidf_norm)
+
+doc_topic_dist = lda.transform(tfidf_norm)
+
+# Get the fitted LDA model
+lda = LatentDirichletAllocation(n_components=10)
+lda.fit(tfidf_norm) 
+
+# Get the word-topic distribution matrix
+topic_word_dist = lda.components_  
+
+# Top 10 words per topic
+n_top_words = 10
+feature_names = tfidf.get_feature_names()
+
+for topic_idx, topic in enumerate(topic_word_dist):
+    top_word_idxs = topic.argsort()[:-n_top_words - 1:-1]
+    top_words = [feature_names[i] for i in top_word_idxs]
+    print(f"Topic {topic_idx}: {', '.join(top_words)}")
+
+
+# top topics for comments
+n_top_topics = 2
+for i, dists in enumerate(doc_topic_dist):
+    topic_idx = dists.argsort()[:-n_top_topics-1:-1]
+    top_topics = [topic_idx[i] for i in range(n_top_topics)]
+    print(f"Document {i}: {top_topics}")
+    break    
+
+# topic distribution
+topic_df = pd.DataFrame(doc_topic_dist, columns=[f"topic_{x}" for x in range(10)])
+topic_df['dominant_topic'] = topic_df.idxmax(1).values
+topic_df['dom_topic_weight'] = topic_df.apply(lambda x: x.loc[x.dominant_topic], axis=1)
+topic_df.dominant_topic.value_counts()
+topic_df.dom_topic_weight.describe()
+topic_df = topic_df[topic_df.dom_topic_weight>0.1].copy()
+# removing non-english comments
+
+print(today_str)
---
+++ analysis/comments_analysis.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 10/13/23
+
+@author: Shahryar Doosti
+
+Analyzing youtube comments
+"""
+
+#%%
+import pandas as pd
+import os
+from datetime import date
+
+
+today = date.today()
+today_str = today.strftime("%m%d%y")
+
+PATH = os.path.abspath(os.getcwd())
+FILE = "merged_comments.csv"
+
+def data_file_path(file_path):
+    return os.path.join(PATH.replace("API","Data"),file_path)
+
+file_path = data_file_path(FILE)
+comments = pd.read_csv(file_path)
+comments.shape
+
+#%%
+# goal to exclude the comments left by the creator
+videos = pd.read_csv(file_path.replace("merged_comments","videos_101223"))
+
+# matching with the creator id
+comments['creator_id'] = comments.merge(videos,how='left',on='video_id').creator_id.values
+# identifying the number of videos a user commented for a creator
+comments['no_videos_commented'] = comments.groupby(['creator_id','user_id']).comment_text.transform('count').values
+# finding the comments left by users with maximum comments per creator
+idx = comments.groupby('video_id').no_videos_commented.transform('max') == comments.no_videos_commented
+comments[idx].comment_text.sample() # not good. didn't work. picked up many user comments
+
+#%% a different approach
+
+# calculate the max number of videos
+comments['no_videos'] = comments.groupby('creator_id').video_id.transform('nunique').values
+# summary of number of videos posted by creators
+comments.groupby('creator_id').no_videos.first().sort_values()
+idx = comments.groupby('video_id').no_videos_commented.transform('max') >= comments.no_videos
+comments[idx].comment_text.sample().iloc[0] # this one didn't seem to work either :(
+comments.groupby(['creator_id','user_id']).video_id.nunique().hist(bins=30)
+
+#%%
+import re
+import numpy as np
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from nltk.corpus import stopwords
+from collections import Counter
+
+print('Basic cleaning ...')     
+comments = comments[comments.comment_text.notnull()].copy()    
+data = comments.comment_text.values.tolist()
+
+# not removing emails, hashtags, and urls as they will be removed anyway
+# unless they are important
+
+# Remove new line characters
+data = [re.sub('\s+', ' ', sent) for sent in data]
+               
+# Remove 's
+data = [re.sub("(\'s)","",sent) for sent in data]
+               
+# Remove distracting single quotes
+data = [re.sub("\'", "", sent) for sent in data]
+
+# Remove extended stop words
+# exstopwords = set(extended_stopwords)
+# data = [' '.join([w for w in sent.split() if not w.lower() in exstopwords]) for sent in data]
+
+docs = [x.strip() for x in data] 
+comments['comment_id'] = np.arange(comments.shape[0])
+doc_ids = comments.comment_id.tolist()  
+
+# combined stopwords
+swlist = list(STOP_WORDS)
+swlist.extend(stopwords.words('english'))
+STOP_WORDS = set(swlist)
+
+#%% spacy
+nlp = spacy.load('en_core_web_sm')        
+
+print('Processing ...')         
+processed_docs = []    
+for doc in nlp.pipe(docs):
+    # Process document using Spacy NLP pipeline.
+    
+    ents = doc.ents  # Named entities.
+
+    # Keep only words (no numbers, no punctuation).
+    # Lemmatize tokens, remove punctuation and remove stopwords.
+    doc = [token.lemma_.lower().strip() for token in doc if token.is_alpha and not token.is_stop  and token.lemma_ != '-PRON-']
+
+    # Add named entities, but only if they are a compound of more than word.
+    doc.extend([str(entity) for entity in ents if len(entity) > 1])
+    
+    processed_docs.append(doc)
+    
+docs = processed_docs.copy()
+print(len(docs))
+#del processed_docs
+
+print('Saving the text ...')  
+processed_path = file_path.replace("merged_comments.csv","processed_comments_102423.txt")       
+with open(processed_path,'w') as f:    
+    for doc in docs:
+        f.write(','.join(filter(lambda x: x not in ['',' ','[]','[ ]'],doc))+'\n')
+        
+
+print('Cleaning Process Completed!')
+# %%
