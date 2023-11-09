dccab882a717473483dbf742a8c0d67b3af3c3df
sdoosti <shahryar65@gmail.com>
2023-11-08 22:15:35 -0800

    update data file versions

--- analysis/cluster_cuml.py
+++ analysis/cluster_cuml.py
@@ -15,16 +15,14 @@ with open(os.path.join(DATA_PATH,"processed_comments_102423.txt"),"r", encoding=
 length = [len(re.sub("\d+", "", x.strip()).split(',')) for x in processed_docs]
 comments = cudf.read_csv(os.path.join(DATA_PATH, "merged_comments.csv"))
 comments = comments[comments.comment_text.notnull()].copy()
-#comments['processed_text'] = 
 comments['length'] = length
 comments['include'] = comments.length > 10
 comments = comments[comments.include].copy()
 print(comments.shape)
 
 # Load the embeddings
-embed_file = "bert_embeddings_221979docs_sentence_lowercase_071123.npy"
-#embed_file = "bert_embeddings_221979docs_sentence_original_071123.npy"
-#embed_file = "bert_embeddings_221979docs_sentence_tokens_071123.npy"
+version = 'tokens' # lowercase, original, or tokens
+embed_file = f"bert_embeddings_221979docs_sentence_{version}_071123.npy"
 
 embeddings = np.load(os.path.join(DATA_PATH, embed_file), allow_pickle=True)
 #gpu = numba.cuda.to_device(embeddings)
@@ -33,7 +31,6 @@ print(len(embd_dict))
 df = cudf.DataFrame(embd_dict)
 
 print(df.shape)
-
 
 print("Kmeans Clustering")
 # Specify the number of clusters
@@ -50,19 +47,22 @@ comments['cluster'] = model.predict(df)
 
 print(comments.head())
 
-comments.to_csv(os.path.join(DATA_PATH, "comments_cluster.csv"),index=False)
+comments.to_csv(os.path.join(DATA_PATH, f"comments_cluster_{version}.csv"),index=False)
 
 print("t-SNE Analysis")
 # Specify the number of components
 n_components = 2
 
 # Create a TSNE model
-tsne = TSNE(n_components=n_components)
+perplexity = 50
+n_neighbors = 500
+tsne = TSNE(n_components=n_components, perplexity=perplexity, n_neighbors=n_neighbors)
 
 # Fit the model to the data and transform it
 df_transformed = tsne.fit_transform(df)
 
 print(df_transformed.head())
-df_transformed.to_csv(os.path.join(DATA_PATH, "tsne.csv"),index=False)
+df_transformed.to_csv(os.path.join(DATA_PATH, f"tsne_{version}_perp{perplexity}_n{n_neighbors}.csv"),index=False)
+
 
 
