e76e6552ceddc6740c0a51fea5a3b76cdd56f5d2
sdoosti <shahryar65@gmail.com>
2023-11-10 11:28:53 -0800

    add a function to create channels demographics

--- utils/convert_creator_json2csv.py
+++ utils/convert_creator_json2csv.py
@@ -49,10 +49,10 @@ def create_csv_row(creator_dict):
                     
     """
     gid = creator_dict['accounts']['youtube']['gid']
-    for account in creator_dict['accounts']:
-        if account['platform'] == 'youtube':
-            gid = account['gid']
-            break
+    # for account in creator_dict['accounts']:
+    #     if account['platform'] == 'youtube':
+    #         gid = account['gid']
+    #         break
     return([creator_dict['creator_id'],
             creator_dict['title'],
             creator_dict['type'],
@@ -64,6 +64,49 @@ def create_csv_row(creator_dict):
             creator_dict['uploads_90d'],
             creator_dict['views'],
             creator_dict['last_upload_date']])
+
+def create_demographics_csv(creator_dict):
+    rows = []
+    for creator in creator_dict:
+        gid = creator['accounts']['youtube']['gid']
+        if 'demographics' not in creator['accounts']['youtube']:
+            rows.append([gid, None, None, None, None, None, None, None, None, None, None])
+            continue
+
+        if creator['accounts']['youtube']['demographics']['ages'] is not None:
+            ages_13_17 = creator['accounts']['youtube']['demographics']['ages'].get('ages_13_17', None)
+            ages_18_24 = creator['accounts']['youtube']['demographics']['ages'].get('ages_18_24', None)
+            ages_25_34 = creator['accounts']['youtube']['demographics']['ages'].get('ages_25_34', None)
+            ages_35_44 = creator['accounts']['youtube']['demographics']['ages'].get('ages_35_44', None)
+            ages_45_54 = creator['accounts']['youtube']['demographics']['ages'].get('ages_45_54', None)
+            ages_55_plus = creator['accounts']['youtube']['demographics']['ages'].get('ages_55_plus', None)
+        else:
+            ages_13_17 = None
+            ages_18_24 = None
+            ages_25_34 = None
+            ages_35_44 = None
+            ages_45_54 = None
+            ages_55_plus = None
+
+        if creator['accounts']['youtube']['demographics']['genders'] is not None:
+            female = creator['accounts']['youtube']['demographics']['genders'].get('female', None)
+            male = creator['accounts']['youtube']['demographics']['genders'].get('male', None)
+        else:
+            female = None
+            male = None
+        if creator['accounts']['youtube']['demographics']['locations'] is not None:
+            US = creator['accounts']['youtube']['demographics']['locations'].get('US', None)
+            CA = creator['accounts']['youtube']['demographics']['locations'].get('CA', None)
+        else:
+            US = None
+            CA = None
+        row = [gid, ages_13_17, ages_18_24, ages_25_34, ages_35_44, ages_45_54, ages_55_plus,
+                female, male, US, CA]
+        rows.append(row)
+    columns = ['creator_id', 'ages_13_17', 'ages_18_24', 'ages_25_34', 'ages_35_44', 'ages_45-54', 'ages_55_plus',
+               'female', 'male', 'US', 'CA']
+    df = pd.DataFrame(data=rows, columns=columns)
+    df.to_csv(os.path.join(os.path.dirname(PATH),"Data","demographics.csv"), index=False)
 
 def create_csv(creator_list):
     data_list = []
@@ -88,6 +131,7 @@ def main(file):
     creator_list = create_csv(creator_dict)
     creator_df = create_dataframe(creator_list)
     save_data(creator_df,data_path)
+    create_demographics_csv(creator_dict)
 
 if __name__ == '__main__':
     YOGA_FILE = "final_yoga_channels_info_091823.json"
